b79838563f6583ff51429cdca530bce8520a373b
Bryant Vail <Bryanttv@outlook.com>
2018-07-08 18:09:42 -0400

    end

--- app.js
+++ app.js
@@ -1,12 +1,35 @@
 //app.js
 
-var myFriend = "Zach Varnes";
+// var myFriend = "Zach Varnes";
 
-console.log(myFriend);
-
-
+// console.log(myFriend);
 
 
 
+let number = 1;
+let message = "hello";
+let is_over_21 = false;
+let = function(a,b){
+    return a+b;
+};
 
+let myFriends = ["John", "Zach", "Lewis", "taj"];
+
+for(let i = 0; i<myFriends.length ; i++){
+    console.log(myFriends[i] + "is my friend");
+}
+console.log(myFriends[0]);
+
+
+
+
+// app.get("/", (req,res) => {
+//     res
+// });
+// let fn = function(a,b){
+//     //code
+// };
+// app.get("/", function(req,res){
+//     //code
+// });
 
