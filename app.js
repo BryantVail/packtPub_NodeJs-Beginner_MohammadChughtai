def4ff5a12ab39785816f47554841dffab527764
Bryant Vail <Bryanttv@outlook.com>
2018-07-10 00:41:07 -0400

    response.write(request["url"]);

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 //app.js
 const http      = require("http");
+const fs        = require("file-system");
 const keys      = require("./config/keys");
 
 http.createServer(function(request, response){
-
+    response.write(request["url"]);
+    response.end();
 }).listen(keys.server.port);
 
 
