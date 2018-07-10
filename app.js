4ecd24eb9e65939d02f07bdbfc41e0df6e2f30c8
Bryant Vail <Bryanttv@outlook.com>
2018-07-10 01:02:38 -0400

    complete /moviesA

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const http      = require("http");
 const fs        = require("file-system");
 const keys      = require("./config/keys");
 
-http.createServer(function(request, response){
-    response.write(request["url"]);
-    response.end();
+http.createServer(function(req, res){
+    try{
+        let file =fs.readFileSync("./movies/"+req["url"]+".txt","utf8");//./babydriver.txt
+        res.write(file);
+    }catch(err){
+        console.log(err);
+        res.write("error 404, file not found");
+    }
+    
+    res.end();
 }).listen(keys.server.port);
 
 
