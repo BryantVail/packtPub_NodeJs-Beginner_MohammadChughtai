938147d7ccb34a7546ab0da80279d7aade07ecb0
Bryant Vail <Bryanttv@outlook.com>
2018-07-10 00:15:30 -0400

    http server has req,res & port #

--- app.js
+++ app.js
@@ -1,63 +1,10 @@
 //app.js
-const serverConfig  = require("./config/keys"); 
-const express       = require("express");
-const app           = express();
-const messages      = require("./messages");
-const fs            = require("fs");
-const bodyParser    = require("body-parser");
+const http      = require("http");
+const keys      = require("./config/keys");
 
-app.use(bodyParser.urlencoded({extended:true}));
-app.use(bodyParser.json());
+http.createServer(function(request, response){
 
-app.use(express.static(__dirname+"/"));
-
-app.get("/", function(req,res){
-    fs.readFile(__dirname+"/index.html",function(err, data){
-        if(!err){
-            res.write(data);
-
-        }
-        res.end();
-    }).catch(function(err){
-        console.log(err);
-    });
-});
-
-app.get("/status", function(req,res){
-    fs.readFile(__dirname+"/posts.json", function(err, data){
-        if(!err){
-            res.send(JSON.parse(data));
-        }else{
-            console.log(err);
-        }
-    });
-});
-
-app.post("/status/new", function(req,res){
-    let status = JSON.stringify(
-        {
-            name:req["body"]["name"],
-            status:req["body"]["status"]
-        }
-    );
-
-    fs.writeFile(__dirname + "/posts.json",
-    status,
-    function(err){
-            console.log(err);
-        }
-    );
-    res.redirect("/");
-});
-
-app.listen(serverConfig.server.port, function(err){
-    if(err){
-        console.log("err: "+err);
-    }else{
-        console.log("listening on port: " + serverConfig.server.port);
-    }
-});
-
+}).listen(keys.server.port);
 
 
 
